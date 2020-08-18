14844f8cf8b1d073211f0f272ef8af887b4fa16a
jlbuild <juliabuildbot@gmail.com>
2020-08-18 09:35:05 +0000

    GDAL_jll build 3.0.4+2

--- src/wrappers/i686-linux-gnu-cxx03.jl
+++ src/wrappers/i686-linux-gnu-cxx03.jl
@@ -629,8 +629,6 @@ function __init__()
     foreach(p -> append!(PATH_list, p), (GEOS_jll.PATH_list, PROJ_jll.PATH_list, Zlib_jll.PATH_list, SQLite_jll.PATH_list, LibCURL_jll.PATH_list, OpenJpeg_jll.PATH_list,))
     foreach(p -> append!(LIBPATH_list, p), (GEOS_jll.LIBPATH_list, PROJ_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, SQLite_jll.LIBPATH_list, LibCURL_jll.LIBPATH_list, OpenJpeg_jll.LIBPATH_list,))
 
-    # Lastly, we need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     global gdal_contour_path_path = normpath(joinpath(artifact_dir, gdal_contour_path_splitpath...))
 
     push!(PATH_list, dirname(gdal_contour_path_path))
@@ -699,12 +697,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
+    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
