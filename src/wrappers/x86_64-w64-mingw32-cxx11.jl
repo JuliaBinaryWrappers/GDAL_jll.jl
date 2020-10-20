7f8d6f85a823be5b57965b43f1cb9c11f37cf45f
jlbuild <juliabuildbot@gmail.com>
2020-10-20 16:21:57 +0000

    GDAL_jll build 3.0.4+4

--- src/wrappers/x86_64-w64-mingw32-cxx11.jl
+++ src/wrappers/x86_64-w64-mingw32-cxx11.jl
@@ -8,6 +8,8 @@ using SQLite_jll
 using LibCURL_jll
 using OpenJpeg_jll
 using Expat_jll
+using Zstd_jll
+using libwebp_jll
 using LibSSH2_jll
 using MbedTLS_jll
 using nghttp2_jll
@@ -33,7 +35,7 @@ JLLWrappers.@declare_executable_product(ogrinfo_path)
 JLLWrappers.@declare_executable_product(ogrlineref_path)
 JLLWrappers.@declare_executable_product(ogrtindex_path)
 function __init__()
-    JLLWrappers.@generate_init_header(GEOS_jll, PROJ_jll, Zlib_jll, SQLite_jll, LibCURL_jll, OpenJpeg_jll, Expat_jll, LibSSH2_jll, MbedTLS_jll, nghttp2_jll)
+    JLLWrappers.@generate_init_header(GEOS_jll, PROJ_jll, Zlib_jll, SQLite_jll, LibCURL_jll, OpenJpeg_jll, Expat_jll, Zstd_jll, libwebp_jll, LibSSH2_jll, MbedTLS_jll, nghttp2_jll)
     JLLWrappers.@init_executable_product(
         gdal_contour_path,
         "bin\\gdal_contour.exe",
