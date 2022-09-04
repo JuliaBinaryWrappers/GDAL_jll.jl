e5c197d21247436c8a657d9ac92cf42346dba710
jlbuild <juliabuildbot@gmail.com>
2022-09-04 20:35:01 +0000

    GDAL_jll build 300.500.101+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -36,7 +36,7 @@ function __init__()
     JLLWrappers.@generate_init_header(GEOS_jll, PROJ_jll, Zlib_jll, SQLite_jll, OpenJpeg_jll, Expat_jll, Zstd_jll, Libtiff_jll, libgeotiff_jll, LibCURL_jll)
     JLLWrappers.@init_library_product(
         libgdal,
-        "lib/libgdal.31.dylib",
+        "lib/libgdal.31.0.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
